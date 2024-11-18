b4bacd7591e2abda7815da4a2a3d8d1249fa0e55
Justina <142683103+justyinaa@users.noreply.github.com>
2024-11-18 16:42:43 +0100

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -57,7 +57,7 @@ const Footer: React.FC = () => {
         </div>
 
         <p className="footerParagraph">
-          2022 Meubel House. All rights reserved
+          2024 Justina Ominisan. All rights reserved.
         </p>
       </>
     );
